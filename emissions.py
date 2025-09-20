b2b389a78c12b1e600d478d7db7cbd3eda1d758f
sandy1983jsr <sandeep.uce208@gmail.com>
2025-09-20 19:16:49 +0530

    Update emissions.py

--- emissions.py
+++ emissions.py
@@ -1,45 +1,47 @@
 import pandas as pd
 
-def calculate_schedule_emissions(equipment, details, switchover, production, steam_ef, elec_ef):
-    # Merge production with details to get relevant info
-    merged = production.merge(details, on='product_code', suffixes=('', '_details'))
-
-    # Emissions for production
+def calculate_batch_emissions(
+    batch_schedule: pd.DataFrame,
+    details: pd.DataFrame,
+    switchover: pd.DataFrame,
+    steam_ef: float,
+    elec_ef: float
+) -> pd.DataFrame:
+    # Attach details for each batch
+    merged = batch_schedule.merge(details, on='product_code', how='left')
     merged['emissions_electricity'] = merged['total_electricity_consumed'] * elec_ef
     merged['emissions_steam'] = merged['total_steam_consumed'] * steam_ef
 
-    # Switchover emissions
-    switch_emissions = []
-    prev_prod = None
+    switchover_electricity = []
+    switchover_steam = []
     for i, row in merged.iterrows():
         if i == 0:
-            switch_emissions.append({'emissions_electricity': 0, 'emissions_steam': 0, 'switchover_type': None})
+            switchover_electricity.append(0)
+            switchover_steam.append(0)
         else:
             curr_prod = row['product_code']
             prev_prod = merged.iloc[i-1]['product_code']
-            if curr_prod == prev_prod:
-                sw_type = 'batch'
-            else:
-                sw_type = 'product'
-            sw_row = switchover[(switchover['product_code'] == curr_prod) & (switchover['switch_type'] == sw_type)]
+            sw_type = 'batch' if curr_prod == prev_prod else 'product'
+            sw_row = switchover[
+                (switchover['product_code'] == curr_prod) & (switchover['switch_type'] == sw_type)
+            ]
             if not sw_row.empty:
-                sw_elec = sw_row['electricity'].values[0]
-                sw_steam = sw_row['steam'].values[0]
+                sw_elec = sw_row.iloc[0]['electricity']
+                sw_steam = sw_row.iloc[0]['steam']
             else:
                 sw_elec = 0
                 sw_steam = 0
-            switch_emissions.append({
-                'emissions_electricity': sw_elec * elec_ef,
-                'emissions_steam': sw_steam * steam_ef,
-                'switchover_type': sw_type
-            })
-    switchover_df = pd.DataFrame(switch_emissions)
-    merged['switchover_emissions_electricity'] = switchover_df['emissions_electricity']
-    merged['switchover_emissions_steam'] = switchover_df['emissions_steam']
-    merged['switchover_type'] = switchover_df['switchover_type']
-
-    merged['total_emissions'] = merged['emissions_electricity'] + merged['emissions_steam'] + merged['switchover_emissions_electricity'] + merged['switchover_emissions_steam']
+            switchover_electricity.append(sw_elec * elec_ef)
+            switchover_steam.append(sw_steam * steam_ef)
+    merged['switchover_emissions_electricity'] = switchover_electricity
+    merged['switchover_emissions_steam'] = switchover_steam
+    merged['total_emissions'] = (
+        merged['emissions_electricity'] +
+        merged['emissions_steam'] +
+        merged['switchover_emissions_electricity'] +
+        merged['switchover_emissions_steam']
+    )
     return merged
 
-def total_emissions(df):
-    return df['total_emissions'].sum()
+def total_facility_emissions(emissions_df: pd.DataFrame) -> float:
+    return emissions_df['total_emissions'].sum()
